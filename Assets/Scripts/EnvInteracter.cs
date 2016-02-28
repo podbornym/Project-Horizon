a226a4d30548ac46bb4bf022c64a777f89de251b
Erik Gunderson <gundy.goo@gmail.com>
2016-02-28 15:05:50 -0600

    Environment Interactor script

---
+++ Assets/Scripts/EnvInteracter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvInteracter : MonoBehaviour {
+
+    public Texture2D normalCursor;
+    public Texture2D mouseOverCursor;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.SetCursor(normalCursor, hotSpot, cursorMode);
+	}
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(mouseOverCursor, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(normalCursor, hotSpot, cursorMode);
+    }
+}
