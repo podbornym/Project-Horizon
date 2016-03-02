0ec5716d2cc7e7485960d7f58f9d8820495c88b5
Erik Gunderson <gundy.goo@gmail.com>
2016-03-02 13:45:16 -0600

    environment scene organization
    zones, painters, and works  organized

--- Assets/Scripts/ObjectMoveScript.cs
+++ Assets/Scripts/ObjectMoveScript.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class ObjectMoveScript : MonoBehaviour {
     public GameObject player;
     public bool mousePressed = false;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(mousePressed)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mousePressed)
         {
             player.transform.position = Vector2.MoveTowards(player.transform.position, transform.position, Time.deltaTime * 10);
         }
-	}
+    }
 
     void OnMouseDown()
     {
