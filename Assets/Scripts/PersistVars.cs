6ed3c78b69b3e492c792f6f018af059af59b2319
Erik Gunderson <gundy.goo@gmail.com>
2016-03-02 17:33:02 -0600

    moar changez

---
+++ Assets/Scripts/PersistVars.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistVars : MonoBehaviour {
+    //this script is attached to the UI so
+    //all variables stored in here will persist
+    //across scene changes
+
+    public static string ggScrub = "This will be the best art theft game ever made.  No doubt about it!";
+}
---
+++ Assets/Scripts/testButtonUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class testButtonUI : MonoBehaviour {
+
+    bool expandMenu = false;
+    public Image piece1;
+    public Image piece2;
+    public Image piece3;
+    public Image piece4;
+
+    // Use this for initialization
+    void Start () {
+        piece1.enabled = false;
+        piece2.enabled = false;
+        piece3.enabled = false;
+        piece4.enabled = false;
+        Vector3 piece1.rectTransform.position + new Vector3(0, 20, 0);
+    }
+	
+	public void ExpandMenu()
+    {
+        if (!expandMenu)
+        {
+            expandMenu = true;
+        }
+        else
+        {
+            expandMenu = false;
+        }
+    }
+
+    void Update()
+    {
+        if(expandMenu)
+        {
+
+            piece1.enabled = true;
+            piece1.rectTransform.position = Vector3.MoveTowards(piece1.rectTransform.position, piece1.rectTransform.position + new Vector3(0, 5, 0), Time.deltaTime * 100);
+        }
+    }
+}
