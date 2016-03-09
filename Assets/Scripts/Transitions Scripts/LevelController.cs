c6c207023abdf753c5b72eecf7f5247ea122b50d
Jacob Gutsch <jgutsch95@gmail.com>
2016-03-09 16:26:54 -0600

    Small script cleaning
    Removed empty method in LevelController.cs

--- Assets/Scripts/Transitions Scripts/LevelController.cs
+++ Assets/Scripts/Transitions Scripts/LevelController.cs
@@ -4,10 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (Input.anyKey){
