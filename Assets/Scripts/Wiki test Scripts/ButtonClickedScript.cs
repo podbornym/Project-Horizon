fc3c860003721fc3848afb332dd3dea2ee2244c0
joshxdaniels <joshxdaniels@gmail.com>
2016-04-06 15:42:55 -0500

    Wiki stuff is clickable

---
+++ Assets/Scripts/Wiki test Scripts/ButtonClickedScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonClickedScript : MonoBehaviour {
+
+    public GameObject Popout1;
+    public GameObject Popout2;
+    public GameObject Popout3;
+    public Text TextField1;
+    public int buttonNumber;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void IAmClicked()
+    {
+        print("Button " + buttonNumber + " clicked");
+        TextField1.text = "Button number " + buttonNumber + " clicked";
+    }
+}
