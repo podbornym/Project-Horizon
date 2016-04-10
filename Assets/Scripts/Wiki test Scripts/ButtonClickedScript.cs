2c7757e1f592ba2416ea1a28b9ee5ec7af8991e1
joshxdaniels <joshxdaniels@gmail.com>
2016-04-10 15:08:55 -0500

    Added art related to Tracing Game

--- Assets/Scripts/Wiki test Scripts/ButtonClickedScript.cs
+++ Assets/Scripts/Wiki test Scripts/ButtonClickedScript.cs
@@ -6,10 +6,12 @@ public class ButtonClickedScript : MonoBehaviour {
 
     public Text TextField1;
     public int buttonNumber;
+    public string buttonName;
 
     public void IAmClicked()
     {
         print("Button " + buttonNumber + " clicked");
-        TextField1.text = "Button number " + buttonNumber + " clicked";
+        //TextField1.text = "Button number " + buttonNumber + " clicked";
+        TextField1.text = "Please select from the " + buttonName + " table of contents";
     }
 }
