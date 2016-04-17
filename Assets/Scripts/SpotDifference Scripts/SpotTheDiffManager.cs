9671b9d3221861b865ee08c47f49c5475e7dbb5f
MegaSamurott503 <kk.mudkip@gmail.com>
2016-04-17 16:17:00 -0500

    Spot the Diff UI Done

--- Assets/Scripts/SpotDifference Scripts/SpotTheDiffManager.cs
+++ Assets/Scripts/SpotDifference Scripts/SpotTheDiffManager.cs
@@ -12,12 +12,18 @@ public class SpotTheDiffManager : MonoBehaviour
     public static bool foundAll = false;
     public Text howManyFound;
     public static Text timeCounter;
+    public Text timeUp;
     public List<GameObject> possibleList;
+    public int score;
+    public Sprite gradeA;
+    public Sprite gradeB;
+    public Sprite gradeC;
+    public Sprite gradeD;
 
     // Use this for initialization
     void Start()
     {
-        seconds = 60;
+        seconds = 90;
         // Get the UI text components
         howManyFound = GameObject.Find("FoundDiffText").GetComponent<Text>();
         timeCounter = GameObject.Find("TimerText").GetComponent<Text>();
@@ -93,17 +99,20 @@ public class SpotTheDiffManager : MonoBehaviour
         if (seconds <= 0 && !foundAll)
         {
             seconds = 0;
-            timeCounter.text = "Time's up!";
-            timeCounter.color = Color.red;
+            timeCounter.text = "";
+            timeUp.text = "Time's up!";
+            timeUp.color = Color.red;
+            FinalScore();
         }
 
         // Acknowledge when the player finds every difference
         if (foundDiff == 5)
         {
             foundAll = true;
-            timeCounter.text = "You win!";
+            timeCounter.text = "";
+            timeUp.text = "You win!";
             penaltyTime = -1;
-            timeCounter.color = Color.white;
+            FinalScore();
         }
 
     }
@@ -111,5 +120,52 @@ public class SpotTheDiffManager : MonoBehaviour
     public void AddDifference(int value)
     {
         foundDiff = value;
+    }
+
+    public void FinalScore()
+    {
+        // The player found all 5 mistakes
+        if (foundAll)
+        {
+            // The player took less than a minute: score 100 / grade A
+            if (seconds > 30)
+            {
+                GameObject.Find("GradeUI").GetComponent<Image>().enabled = true;
+                GameObject.Find("GradeUI").GetComponent<Image>().sprite = gradeA;
+                score = 100;
+            }
+            // The player took more than a minute: score 90 / grade A
+            else
+            {
+                GameObject.Find("GradeUI").GetComponent<Image>().enabled = true;
+                GameObject.Find("GradeUI").GetComponent<Image>().sprite = gradeA;
+                score = 90;
+            }
+        }
+        // The player ran out of time
+        else
+        {
+            // Only 4 differences were found: score 80 / grade B
+            if (foundDiff == 4)
+            {
+                GameObject.Find("GradeUI").GetComponent<Image>().enabled = true;
+                GameObject.Find("GradeUI").GetComponent<Image>().sprite = gradeB;
+                score = 80;
+            }
+            // Only 3 differences were found: score 70 / grade C
+            else if (foundDiff == 3)
+            {
+                GameObject.Find("GradeUI").GetComponent<Image>().enabled = true;
+                GameObject.Find("GradeUI").GetComponent<Image>().sprite = gradeC;
+                score = 70;
+            }
+            // 2 or fewer differences were found: score 60 / grade D
+            else if (foundDiff <= 2)
+            {
+                GameObject.Find("GradeUI").GetComponent<Image>().enabled = true;
+                GameObject.Find("GradeUI").GetComponent<Image>().sprite = gradeD;
+                score = 60;
+            }
+        }
     }
 }
