d4e159bf917a6df80b0b2fcd59d4279c4233f92a
Jacob Gutsch <jgutsch95@gmail.com>
2016-04-20 16:45:20 -0500

    Minor Selling update

--- Assets/Scripts/DialogueReader.cs
+++ Assets/Scripts/DialogueReader.cs
@@ -429,6 +429,7 @@ public class DialogueReader : MonoBehaviour
 						blkPrice = SLog.BkPay (SellCont.counter, SellCont.correct, SellCont.maxValue);
 						currentText.text = "Congradulations! You sold the forgery for $" + blkPrice;
 						PersistVars.currentMoney += blkPrice;
+						GoTo ("Continue");
 						break;
 					case "#quest1":
 						if (PersistVars.paintingNum == 4) // Shoki Striding
