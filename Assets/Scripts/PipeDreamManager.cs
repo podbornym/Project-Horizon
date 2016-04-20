fc15895acfb81c3dbe12d1b630e5321fbf070b23
FuzzyMoney <polegomaniac@yahoo.com>
2016-04-20 16:48:17 -0500

    Pipe Dream works for real
    I lied last time, whoops

--- Assets/Scripts/PipeDreamManager.cs
+++ Assets/Scripts/PipeDreamManager.cs
@@ -16,6 +16,7 @@ public class PipeDreamManager : MonoBehaviour {
     public PipeRotator rotator;
 
     public bool gameRunning = true;
+    public bool hitTile = true;
     //public int[] randomAngles = new int[4] { 0, 90, 180, 270 };
 
 
@@ -90,7 +91,7 @@ public class PipeDreamManager : MonoBehaviour {
                 if (objectGrid[i].GetComponent<PipeRotator>().gameObject.tag == "Straight")
                 {
                     //If piece is vertically alligned
-                    if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 270) || Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 90))
+                    if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 270 ||  Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 90)
                     {
                         //Calls CheckUp and CheckDown
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckUpFull() || objectGrid[i].GetComponent<PipeRotator>().CheckDownFull())
@@ -98,12 +99,13 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
                     }
                     //If piece is horizontally alligned
-                    else if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 180) || Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 0))
+                    else if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 180 ||  Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 0)
                     {
                         //Calls CheckLeft and CheckRight
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckLeftFull() || objectGrid[i].GetComponent<PipeRotator>().CheckRightFull())
@@ -111,6 +113,7 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
@@ -120,7 +123,7 @@ public class PipeDreamManager : MonoBehaviour {
                 else if (objectGrid[i].GetComponent<PipeRotator>().gameObject.tag == "Turn")
                 {
                     //If piece goes from Left to Up
-                    if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 270))
+                    if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 270)
                     {
                         //Calls CheckLeft and CheckUp
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckLeftFull() || objectGrid[i].GetComponent<PipeRotator>().CheckUpFull())
@@ -128,12 +131,13 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
                     }
                     //If piece goes from up to right
-                    else if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 180))
+                    else if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 180)
                     {
                         //Calls CheckUp and CheckRight
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckUpFull() || objectGrid[i].GetComponent<PipeRotator>().CheckRightFull())
@@ -141,12 +145,13 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
                     }
                     //If piece goes from Right to Down
-                    else if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 90))
+                    else if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 90)
                     {
                         //Calls CheckRight and CheckDown
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckRightFull() || objectGrid[i].GetComponent<PipeRotator>().CheckDownFull())
@@ -154,12 +159,13 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
                     }
                     //If piece goes from Down to Left
-                    else if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 0))
+                    else if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 0)
                     {
                         //Calls CheckDown and CheckLeft
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckDownFull() || objectGrid[i].GetComponent<PipeRotator>().CheckLeftFull())
@@ -167,6 +173,7 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
@@ -176,7 +183,7 @@ public class PipeDreamManager : MonoBehaviour {
                 else if (objectGrid[i].GetComponent<PipeRotator>().gameObject.tag == "FourWay")
                 {
                     //If piece is horizontally alligned
-                    if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 270) || Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 90))
+                    if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 270 ||  Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 90)
                     {
                         //Calls Check*
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckUpFull() || objectGrid[i].GetComponent<PipeRotator>().CheckDownFull() || objectGrid[i].GetComponent<PipeRotator>().CheckLeftFull() || objectGrid[i].GetComponent<PipeRotator>().CheckRightFull())
@@ -184,12 +191,13 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
                     }
                     //If piece is vertically alligned
-                    else if (Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 180) || Mathf.Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z, 0))
+                    else if ( Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 180 ||  Approximately(objectGrid[i].GetComponent<PipeRotator>().transform.rotation.eulerAngles.z) == 0)
                     {
                         //Calls Check*
                         if (objectGrid[i].GetComponent<PipeRotator>().CheckUpFull() || objectGrid[i].GetComponent<PipeRotator>().CheckDownFull() || objectGrid[i].GetComponent<PipeRotator>().CheckLeftFull() || objectGrid[i].GetComponent<PipeRotator>().CheckRightFull())
@@ -197,12 +205,43 @@ public class PipeDreamManager : MonoBehaviour {
                             if (objectGrid[i].GetComponent<PipeRotator>().IsFull == false)
                             {
                                 objectGrid[i].GetComponent<PipeRotator>().IsFull = true;
+                                hitTile = true;
                                 break;
                             }
                         }
                     }
                 }
+                else
+                {
+                    hitTile = false;
+                }
+            }
+            if (hitTile == false)
+            {
+                print("Wow you suck, get good scrub");
+                gameRunning = false;
             }
         }
+    }
+
+    int Approximately(float value)
+    {
+        if ((value < 45 && value >= 0) || (value <= 360 && value > 315))
+        {
+            return 0;
+        }
+        else if (value < 135 && value > 45)
+        {
+            return 90;
+        }
+        else if (value < 225 && value > 135)
+        {
+            return 180;
+        }
+        else if (value < 315 && value > 225)
+        {
+            return 270;
+        }
+        return 123456789;
     }
 }
