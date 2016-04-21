b1095ea2169bfb2387750abb48f5d0a4f1e9bd59
Jacob Gutsch <jgutsch95@gmail.com>
2016-04-21 17:15:43 -0500

    Game Over disables UI

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -3,9 +3,17 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour {
-	
+
+	public GameObject UI;
+
+	void Start ()
+	{
+		UI.GetComponent<Canvas> ().enabled = false;
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if (Input.anyKeyDown)
 		{
 			SceneManager.LoadScene ("mansion");
