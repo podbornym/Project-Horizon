44802d30401073e052392b2ce5a84ec324af4e04
joshxdaniels <joshxdaniels@gmail.com>
2016-04-21 16:26:55 -0500

    Wiki progress

---
+++ Assets/Scripts/Wiki test Scripts/wikiDataScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wikiDataScript : MonoBehaviour {
+
+    public string[] uk1 = new string[6];
+    public string[] uk2 = new string[6];
+    public string[] uk3 = new string[6];
+    public string[] uk4 = new string[6];
+    public string[] uk5 = new string[6];
+    public string[] uk6 = new string[6];
+    public string[] su1 = new string[6];
+    public string[] su2 = new string[6];
+    public string[] su3 = new string[6];
+    public string[] su4 = new string[6];
+    public string[] su5 = new string[6];
+    public string[] su6 = new string[6];
+    public string[] ba1 = new string[6];
+    public string[] ba2 = new string[6];
+    public string[] ba3 = new string[6];
+    public string[] ba4 = new string[6];
+    public string[] ba5 = new string[6];
+    public string[] ba6 = new string[6];
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
