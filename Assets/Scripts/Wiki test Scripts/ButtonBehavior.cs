425fc32a9b7212e57a3b75f07b9c38839880eb69
joshxdaniels <joshxdaniels@gmail.com>
2016-04-22 10:21:35 -0500

    Wiki changes

--- Assets/Scripts/Wiki test Scripts/ButtonBehavior.cs
+++ Assets/Scripts/Wiki test Scripts/ButtonBehavior.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ButtonBehavior : MonoBehaviour {
 
     //public Renderer rend;
-
+    public Text textWindow;
     // Use this for initialization
     void Start () {
         //rend = GetComponent<Renderer>();
@@ -14,10 +15,15 @@ public class ButtonBehavior : MonoBehaviour {
     public void SetVisible()
     {
         if (gameObject.activeSelf == true)
+        {
             gameObject.SetActive(false);
+            textWindow.text = "Please select from the left, and then select a work of art.";
+            print("text should be changed");
+        }
         //rend.enabled = true;
         else
             gameObject.SetActive(true);
+            
 
     }
     public void SetInvisible()
