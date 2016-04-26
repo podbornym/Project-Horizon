7af8cd89b4633bf6133969c01d815a4eb92c93b5
Erik Gunderson <gundy.goo@gmail.com>
2016-04-26 03:08:16 -0500

    All Beginning Menus Complete

---
+++ Assets/Scripts/UI Scripts/MenuButtons.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    public Texture2D newCursor;
+    public CursorMode cursorMode = CursorMode.ForceSoftware;
+    public Vector2 hotSpot = new Vector2(30, 30);
+
+    void Start()
+    {
+        Cursor.SetCursor(newCursor, hotSpot, cursorMode);
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("mansion");
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OverviewButton()
+    {
+        SceneManager.LoadScene("OverView");
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void CreditsNext()
+    {
+        SceneManager.LoadScene("CreditsSecond");
+    }
+}
