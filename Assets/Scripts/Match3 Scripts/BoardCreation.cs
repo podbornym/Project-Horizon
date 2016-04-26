bd95dbb4ac2a994f978d7962a15131fb3470951e
podbornym <podbornym0542@my.uwstout.edu>
2016-04-26 16:51:55 -0500

    match3 bug fixing

--- Assets/Scripts/Match3 Scripts/BoardCreation.cs
+++ Assets/Scripts/Match3 Scripts/BoardCreation.cs
@@ -844,11 +844,11 @@ public class BoardCreation : MonoBehaviour
 			else
 			{
 				//puzzleObject.GetComponent<AudioSource> ().PlayOneShot (wining, 1.0f);
-				match3Return = (.3f - dragPenalty) * 100 + 10;
+				match3Return = (.8f - dragPenalty) * 100 + 10;
 				warningText.color = Color.green;
 				warningText.text = "Your score was: " + match3Return + "%";
 				GameObject.Find("GradeImage").GetComponent<Image>().enabled = true;
-				GameObject.Find("GradeImage").GetComponent<Image>().sprite = D;
+				GameObject.Find("GradeImage").GetComponent<Image>().sprite = A;
 			}
         //}
 
